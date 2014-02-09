51fde6bdacbb4cdce69c4b323d41313fc19fe251
Robert Zuber <rob@zuber.net>
2014-02-09 11:47:29 -0800

    Fix resources migration

--- db/migrate/20140209061847_add_resources.rb
+++ db/migrate/20140209061847_add_resources.rb
@@ -1,10 +1,12 @@
-class AddResources < ActiveRecord::Migration
+class AddVotes < ActiveRecord::Migration
   def change
     create_table :resources do |t|
       t.string :name
       t.string :category
       t.string :address
       t.string :phone
+      t.integer :upvotes, default: 0, null: false
+      t.integer :downvotes, default: 0, null: false
       t.timestamps
     end
   end
