2dc06641ea7bd373f299feceb1ca6497a4979859
Robert Zuber <rob@zuber.net>
2014-02-09 08:40:08 -0800

    Expose resources

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Onevillage::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
+  resources :resources
+
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
